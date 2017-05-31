da37e60b1bef9e5b46f1525baa4af002d992b07d
Brandon Keepers <bkeepers@github.com>
2017-05-31 08:01:24 -0500

    Add standard and fix lint errors

--- index.js
+++ index.js
@@ -1,110 +1,108 @@
 const {TextMessage} = require('hubot')
 
 class Command {
-  constructor(definition, action) {
-    const [name, ...args] = definition.split(/ +/);
+  constructor (definition, action) {
+    const [name, ...args] = definition.split(/ +/)
 
-    this._name = name;
-    this.arguments = args.map(arg => new Argument(arg));
-    this.action = action;
+    this._name = name
+    this.arguments = args.map(arg => new Argument(arg))
+    this.action = action
   }
 
-  matches(name) {
-    return name == this._name || name == this._alias;
+  matches (name) {
+    return name === this._name || name === this._alias
   }
 
-  validateArgs(args) {
-    for(let i = 0; i < this.arguments.length; i++) {
-      const arg = this.arguments[i];
-      arg.validate(args[i]);
+  validateArgs (args) {
+    for (let i = 0; i < this.arguments.length; i++) {
+      const arg = this.arguments[i]
+      arg.validate(args[i])
     }
   }
 
-  description(str) {
+  description (str) {
     if (str === undefined) {
-      return this._description;
+      return this._description
     } else {
-      this._description = str;
-      return this;
+      this._description = str
+      return this
     }
   }
 
-  alias(alias) {
-    if(alias === undefined) {
-      return this._alias;
+  alias (alias) {
+    if (alias === undefined) {
+      return this._alias
     } else {
-      this._alias = alias;
-      return this;
+      this._alias = alias
+      return this
     }
   }
 
-  get help() {
+  get help () {
     let help = `hubot ${this._name}`
-    if(this.arguments.length)
-      help += ' ' + this.arguments.map(arg => arg.help).join(' ');
-    if(this._description)
-      help += ` - ${this._description}`
-    return help;
+    if (this.arguments.length) { help += ' ' + this.arguments.map(arg => arg.help).join(' ') }
+    if (this._description) { help += ` - ${this._description}` }
+    return help
   }
 }
 
 class Argument {
-  constructor(definition) {
-    const match = definition.match(/^([\[<])?([^\]>]+)[\]>]?$/)
-    this._name = match[2];
-    this.required = match[1] == '<';
+  constructor (definition) {
+    const match = definition.match(/^([[<])?([^\]>]+)[\]>]?$/)
+    this._name = match[2]
+    this.required = match[1] === '<'
   }
 
-  validate(arg) {
-    if(this.required && null == arg) {
-      throw `missing required argument \`${this._name}\``;
+  validate (arg) {
+    if (this.required && arg == null) {
+      throw new Error(`missing required argument \`${this._name}\``)
     }
   }
 
-  get help() {
+  get help () {
     return `${this.required ? '<' : '['}${this._name}${this.required ? '>' : ']'}`
   }
 }
 
 module.exports = (robot) => {
-  const commands = [];
-  const regex = robot.respondPattern(/(\w+)(?: (.*))?/);
+  const commands = []
+  const regex = robot.respondPattern(/(\w+)(?: (.*))?/)
 
-  function listener(message) {
-    if(message instanceof TextMessage) {
-      const match = message.match(regex);
-      if(match) {
-        match.command = commands.find(cmd => cmd.matches(match[1]));
-        if(match.command) {
+  function listener (message) {
+    if (message instanceof TextMessage) {
+      const match = message.match(regex)
+      if (match) {
+        match.command = commands.find(cmd => cmd.matches(match[1]))
+        if (match.command) {
           return match
         }
       }
     }
   }
 
-  function handler(res) {
-    const cmd = res.match.command;
-    const args = res.match[2] ? res.match[2].split(/ +/) : [];
+  function handler (res) {
+    const cmd = res.match.command
+    const args = res.match[2] ? res.match[2].split(/ +/) : []
     try {
-      cmd.validateArgs(args);
-      return cmd.action(res, ...args);
-    } catch(error) {
-      res.send(`error: ${error}`);
+      cmd.validateArgs(args)
+      return cmd.action(res, ...args)
+    } catch (error) {
+      res.send(`error: ${error.message}`)
     }
   }
 
-  function command(name, action) {
-    const cmd = new Command(name, action);
+  function command (name, action) {
+    const cmd = new Command(name, action)
     commands.push(cmd)
-    return cmd;
+    return cmd
   }
 
-  robot.listen(listener, handler);
+  robot.listen(listener, handler)
 
   command('help', (res) => {
-    const help  = commands.map(cmd => cmd.help).filter(cmd => cmd);
-    res.send(help.join('\n'));
-  }).description('display help for available commands');
+    const help = commands.map(cmd => cmd.help).filter(cmd => cmd)
+    res.send(help.join('\n'))
+  }).description('display help for available commands')
 
-  return {command};
+  return {command}
 }
--- test/index.js
+++ test/index.js
@@ -1,42 +1,42 @@
 // loading hubot fails without coffeescript
-require("coffee-script");
+require('coffee-script')
 
-const expect = require('expect');
-const commands = require('..');
-const Helper = require('hubot-test-helper');
+const expect = require('expect')
+const commands = require('..')
+const Helper = require('hubot-test-helper')
 
-const helper = new Helper([]);
+const helper = new Helper([])
 
 describe('.command', () => {
-  let cli;
-  let room;
+  let cli
+  let room
 
-  beforeEach(function() {
-    room = helper.createRoom();
-    cli = commands(room.robot);
-  });
+  beforeEach(function () {
+    room = helper.createRoom()
+    cli = commands(room.robot)
+  })
 
-  afterEach(function() {
-    room.destroy();
-  });
+  afterEach(function () {
+    room.destroy()
+  })
 
   it('registers the command', async () => {
-    cli.command('test', res => res.send('hello world'));
+    cli.command('test', res => res.send('hello world'))
 
-    await room.user.say('alice', '@hubot test');
+    await room.user.say('alice', '@hubot test')
 
     expect(room.messages).toEqual([
       ['alice', '@hubot test'],
       ['hubot', 'hello world']
-    ]);
-  });
+    ])
+  })
 
   describe('help', () => {
     it('adds the description to help', async () => {
       cli.command('test <arg>', res => { })
-        .description('a description of the command');
+        .description('a description of the command')
 
-      await room.user.say('alice', '@hubot help');
+      await room.user.say('alice', '@hubot help')
 
       expect(room.messages).toEqual([
         ['alice', '@hubot help'],
@@ -44,13 +44,13 @@ describe('.command', () => {
           'hubot help - display help for available commands',
           'hubot test <arg> - a description of the command'
         ].join('\n')]
-      ]);
-    });
+      ])
+    })
 
     it('shows undocumented commands in help', async () => {
-      cli.command('test <arg>', res => { });
+      cli.command('test <arg>', res => { })
 
-      await room.user.say('alice', '@hubot help');
+      await room.user.say('alice', '@hubot help')
 
       expect(room.messages).toEqual([
         ['alice', '@hubot help'],
@@ -58,83 +58,82 @@ describe('.command', () => {
           'hubot help - display help for available commands',
           'hubot test <arg>'
         ].join('\n')]
-      ]);
-    });
-  });
+      ])
+    })
+  })
 
   describe('with required arguments', () => {
     beforeEach(() => {
       cli.command('hello <name>', (res, name) => {
-        res.send(`${name}, ${res.envelope.user.name} says hello`);
-      });
-    });
+        res.send(`${name}, ${res.envelope.user.name} says hello`)
+      })
+    })
 
     it('passes arguments to callback', async () => {
-      await room.user.say('alice', '@hubot hello bob');
+      await room.user.say('alice', '@hubot hello bob')
 
       expect(room.messages).toEqual([
         ['alice', '@hubot hello bob'],
         ['hubot', 'bob, alice says hello']
-      ]);
-    });
+      ])
+    })
 
     it('returns an error when missing required arguments', async () => {
-      await room.user.say('alice', '@hubot hello');
+      await room.user.say('alice', '@hubot hello')
 
       expect(room.messages).toEqual([
         ['alice', '@hubot hello'],
         ['hubot', 'error: missing required argument `name`']
-      ]);
-    });
-  });
+      ])
+    })
+  })
 
   describe('with optional arguments', () => {
     beforeEach(() => {
       cli.command('goodbye [name]', (res, name) => {
-        res.send(`goodbye ${name || 'world'}`);
-      });
-    });
+        res.send(`goodbye ${name || 'world'}`)
+      })
+    })
 
     it('passes arguments to callback', async () => {
-      await room.user.say('alice', '@hubot goodbye bob');
+      await room.user.say('alice', '@hubot goodbye bob')
 
       expect(room.messages).toEqual([
         ['alice', '@hubot goodbye bob'],
         ['hubot', 'goodbye bob']
-      ]);
-    });
+      ])
+    })
 
     it('does not return an error when optional arguments', async () => {
-      await room.user.say('alice', '@hubot goodbye');
+      await room.user.say('alice', '@hubot goodbye')
 
       expect(room.messages).toEqual([
         ['alice', '@hubot goodbye'],
         ['hubot', 'goodbye world']
-      ]);
-    });
-  });
+      ])
+    })
+  })
 
   describe('alias', () => {
     beforeEach(() => {
-      cli.command('hello', res => res.send('hello world')).alias('hi');
-    });
+      cli.command('hello', res => res.send('hello world')).alias('hi')
+    })
 
     it('invokes command by alias', async () => {
-      await room.user.say('alice', '@hubot hi');
+      await room.user.say('alice', '@hubot hi')
 
       expect(room.messages).toEqual([
         ['alice', '@hubot hi'],
         ['hubot', 'hello world']
-      ]);
-    });
+      ])
+    })
 
     it('does not invoke command with mismatched alias', async () => {
-      await room.user.say('alice', '@hubot hip');
+      await room.user.say('alice', '@hubot hip')
 
       expect(room.messages).toEqual([
         ['alice', '@hubot hip']
-      ]);
-    });
-
-  });
-});
+      ])
+    })
+  })
+})
